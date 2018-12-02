28d04acf2dee3f7c4feeadc137a87f09f72cc46a
Fernanda Reis <fernandafcr@gmail.com>
2018-12-02 20:38:20 -0200

    pg problme

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -39,8 +39,7 @@ class PageController < ApplicationController
 		 	where("EXTRACT( year from dt_start::date) = ? ", y).
       where("EXTRACT( month from dt_start::date) = ? ", m)
 
-puts "teste"
-puts @next_current_employee_job.count
+
 		 	@past_current_employee_job = Job.joins(:employee_jobs).
 		 	where("employee_jobs.employee_detail_id = ?", @current_user_employer_id).
 		 	where("jobs.job_situation_id = '2'").
@@ -48,7 +47,7 @@ puts @next_current_employee_job.count
       where("EXTRACT( year from dt_start::date) = ? ", y).
       where("EXTRACT( month from dt_start::date) = ? ", m).
 		 	select("jobs.*, employee_jobs.employeer_job_situation_id as situation")
-      puts @past_current_employee_job.count
+    
 		 	
 		end
   end 
