ed6b3bc9738cd109f321a7fdf436b6773d9c4c17
Fernanda Reis <fernandafcr@gmail.com>
2018-12-02 14:44:32 -0200

    heroku testing

--- db/migrate/20181123105916_create_jobs.rb
+++ db/migrate/20181123105916_create_jobs.rb
@@ -3,7 +3,7 @@ class CreateJobs < ActiveRecord::Migration[5.2]
     create_table :jobs do |t|
       t.date :dt_start
       t.date :dt_end
-      t.time :meeting_time
+      t.datetime :meeting_time
       t.time :start_time
       t.time :end_time
       t.time :job_end
--- db/migrate/20181202132507_change_meeting_time_to_be_date_time.rb
+++ db/migrate/20181202132507_change_meeting_time_to_be_date_time.rb
@@ -1,5 +1,5 @@
 class ChangeMeetingTimeToBeDateTime < ActiveRecord::Migration[5.2]
   def change
-  	change_column :jobs, :meeting_time, :datetime
+  	
   end
 end
