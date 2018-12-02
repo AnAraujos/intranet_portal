1ff0d9286b736e54072d57abbbf156b863620940
Fernanda Reis <fernandafcr@gmail.com>
2018-12-02 18:04:00 -0200

    group pg

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -10,17 +10,16 @@ class PageController < ApplicationController
   def myjobs
   	if user_signed_in?
 		 	 @current_employee_job_by_month = Job.joins(:employee_jobs).
+		 	 group("extract(year from dt_start)").
 		 	 where("employee_jobs.employee_detail_id = ?", @current_user_employer_id).
-		 	 select("jobs.*, jobs.id, employee_jobs.employee_detail_id as employee, count(*) as count, sum(paid_hours) as paid_hours, sum(travel_hours) as travel_hours, avg(jobs.id), to_char(dt_start,'Mon') as mon, extract(year from dt_start) as yyyy groub BY mon, yyyy, job.id")
+		 	 select("jobs.*, employee_jobs.employee_detail_id as employee, count(*) as count, sum(paid_hours) as paid_hours, sum(travel_hours) as travel_hours")
 		 	 
       @employee = EmployeeDetail.find_by(id: @current_user_employer_id)
 
       @employee_rate = @employee.employee_asset.rate
 
       @travel_rate = 9.90
-      def self.query_by_year_month(y, m)
-    where("extract(year from date) = ? and extract(month from date) = ?", y, m)
-  end
+
 
 
 		end
