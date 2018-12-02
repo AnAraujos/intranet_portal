6de49eb90efe3037679a95294df5b5bc99739b78
Fernanda Reis <fernandafcr@gmail.com>
2018-12-02 19:54:22 -0200

    group by myjobs page controller

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -28,20 +28,22 @@ class PageController < ApplicationController
 
   def jobs_list
   	if user_signed_in?
-      dt = Date.strptime(params[:dt], "%d-%m-%y")
+      dt = Date.strptime(params[:dt], "%m/%y")
       m = dt.strftime("%m")
       y = dt.strftime("%Y")
 		 	@next_current_employee_job = Job.joins(:employee_jobs).
 		 	where("employee_jobs.employee_detail_id = ?", @current_user_employer_id).
 		 	where("job_situation_id = '1'").
 		 	where("employee_jobs.employeer_job_situation_id != '1'").
-		 	where("EXTRACT( year from dt_start::date)::integer = ? ", 2018)
+		 	where("EXTRACT( year from dt_start::date) = ? ", y).
+      where("EXTRACT( month from dt_start::date) = ? ", m)
 
 		 	@past_current_employee_job = Job.joins(:employee_jobs).
 		 	where("employee_jobs.employee_detail_id = ?", @current_user_employer_id).
 		 	where("jobs.job_situation_id = '2'").
 		 	where("employee_jobs.employeer_job_situation_id != '1'").
-      where("EXTRACT( year from dt_start::date)::integer = ? ", 2018).
+      where("EXTRACT( year from dt_start::date) = ? ", y).
+      where("EXTRACT( month from dt_start::date) = ? ", m).
 		 	select("jobs.*, employee_jobs.employeer_job_situation_id as situation")
 		 	
 		end
