ac6afb81c7a84d5d26b823036778a6e1683fb541
Fernanda Reis <fernandafcr@gmail.com>
2018-12-02 17:22:57 -0200

    group by myjobs page controller

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -10,15 +10,17 @@ class PageController < ApplicationController
   def myjobs
   	if user_signed_in?
 		 	 @current_employee_job_by_month = Job.joins(:employee_jobs).
-		 	 group("strftime('%m-%Y', dt_start) ").
 		 	 where("employee_jobs.employee_detail_id = ?", @current_user_employer_id).
-		 	 select("jobs.*, employee_jobs.employee_detail_id as employee, count(*) as count, sum(paid_hours) as paid_hours, sum(travel_hours) as travel_hours")
+		 	 select("to_char(dt_start,'Mon') as mon, extract(year from dt_start) as yyyy, jobs.*, employee_jobs.employee_detail_id as employee, count(*) as count, sum(paid_hours) as paid_hours, sum(travel_hours) as travel_hours group by 1,2")
 		 	 
       @employee = EmployeeDetail.find_by(id: @current_user_employer_id)
 
       @employee_rate = @employee.employee_asset.rate
 
       @travel_rate = 9.90
+      def self.query_by_year_month(y, m)
+    where("extract(year from date) = ? and extract(month from date) = ?", y, m)
+  end
 
 
 		end
