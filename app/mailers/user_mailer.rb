63d29a807b6ec7c00683e95387abab914f59bb4f
Ana Araujo <anaaraujos@hotmail.com.br>
2018-12-06 17:57:43 +0000

    Final Project

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
 	def send_mail_persons(user)
     @user = user
-    mail :to => @user.email, :subject => "Welcome to HR System.", :from => "hr@gmail.com"
+    mail :to => @user.email, :subject => "Welcome to the INTRANET System.", :from => "intranet@gmail.com"
   end
 end 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "hr_project_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "intranet_portal_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
